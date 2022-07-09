69ed225b1287276061516fe253391cb5423b3da0
Luis Spader <luisspaders@gmail.com>
2022-07-08 21:40:08 -0300

    agora

--- Códigos Arduino/Hidroponia_Servidor_WEB_V1/TagoIO_Hidroponia_Servidor_WEB/TagoIO_Hidroponia_Servidor_WEB.ino
+++ Códigos Arduino/Hidroponia_Servidor_WEB_V1/TagoIO_Hidroponia_Servidor_WEB/TagoIO_Hidroponia_Servidor_WEB.ino
@@ -18,6 +18,12 @@ Alimentação: 2x Placa solar 1W // 2x Bateria Samsung LiPo 18650 com 2600mAH //
 #include <nRF24L01.h>
 #include <RF24.h>
 #include <ESP8266WiFi.h>
+//#include <DNSServer.h>
+//#include <ESP8266WebServer.h>
+//#include <WiFiManager.h> //https://github.com/tzapu/WiFiManager
+//#include <OneWire.h>
+//#include <DallasTemperature.h>
+
 // ------------- Mapeamento de Hardware -------------
 #define pin_CE          D14
 #define pin_CSN         D10
@@ -32,11 +38,11 @@ int bateria = 0;
 int regulador = 0;
 char teste[32] = {0};
 // ------------- Outras Configurações -------------
-String apiKey = "3WGO6312X089C3UI";
+String apiKey = "0dcdb8e3-2d69-403d-ab51-0e52aa4ec860"; //o token vai aqui
 const char* ssid = "Thomé Suítes Externas";
 const char* password = "senha123";
 WiFiClient client;
-const char* server = "api.thingspeak.com";
+const char* server = "api.tago.io";
 // Pinout do módulo RF:
 RF24 radio(pin_CE, pin_CSN);
 // Enderelo do canal de comunicação:
@@ -98,36 +104,84 @@ void loop(){
     Serial.println(teste);
     Serial.println("regulador");
     Serial.println(teste);
-    
+
+    //Inicia um client TCP para o envio dos dados
     if (client.connect(server, 80)) { // "184.106.153.149" or api.thingspeak.com
-      String postStr = apiKey;
-      postStr += "&field1=";
-      postStr += String(solar);
-      postStr += "&field2=";
-      postStr += String(bateria);
-      postStr += "&field3=";
-      postStr += String(regulador);
-      postStr += "&field4=";
-      postStr += String(temperatura);
-      postStr += "&field5=";
-      postStr += String(humidade_ar);
-      postStr += "&field6=";
-      postStr += String(humidade_ar);
-      postStr += "&field7=";
-      postStr += String(humidade_ar);
-      postStr += "&field8=";
-      postStr += String(humidade_ar);
-      postStr += "\r\n\r\n";
-      client.print("POST /update HTTP/1.1\n");
-      client.print("Host: api.thingspeak.com\n");
-      client.print("Connection: close\n");
-      client.print("X-THINGSPEAKAPIKEY: " + apiKey + "\n");
-      client.print("Content-Type: application/x-www-form-urlencoded\n");
-      client.print("Content-Length: ");
-      client.print(postStr.length());
-      client.print("\n\n");
-      client.print(postStr);
+       Serial.print("CONNECTED AT TAGO\n"); // conectou no tagoIO
+ 
+    String postStr = "";
+ 
+    
+ 
+    postStr = "POST /data HTTP/1.1\n";
+    postStr += "Host: api.tago.io\n";
+    postStr += "Device-Token: "+apiKey+"\n";
+    postStr += "_ssl: false\n";
+    postStr += "Content-Type: application/x-www-form-urlencoded\n";
+    postStr += "Content-Length: "+String(postData.length())+"\n";
+    postStr += "\n";
+//    postStr += postData;
+
+//      String postStr = apiKey;
+//    String postData = "variable=temp&value="+String(temp)+"\n"; //enviando
+
+
+//solar, bateria, regulador, temperatura, humidade_ar
+      String PostData = "[{\"variable\":\"valor1\", \"value\":\"";
+      PostData += String(solar);
+      PostData = "\"},{\"variable\":\"valor2\",\"value\":\"";      
+      PostData += String(bateria);
+      
+      PostData = "\"},{\"variable\":\"valor3\",\"value\":\"";      
+      PostData += String(regulador);
+
+      PostData = "\"},{\"variable\":\"valor3\",\"value\":\"";      
+      PostData += String(temperatura);
+
+      PostData = "\"},{\"variable\":\"valor3\",\"value\":\"";      
+      PostData += String(humidade_ar);
+
+      PostData = "\"},{\"variable\":\"valor3\",\"value\":\"";      
+      PostData += String(humidade_ar);
+
+      PostData = "\"},{\"variable\":\"valor3\",\"value\":\"";      
+      PostData += String(humidade_ar);
+
+      PostData = "\"},{\"variable\":\"valor3\",\"value\":\"";      
+      PostData += String(humidade_ar);
+
+      PostData += "\"}]";
+ 
+    client.print(postStr);
+    //Serial.print("%d", postStr);
+ 
+    unsigned long timeout = millis();
+    while (client.available() == 0) {
+      if (millis() - timeout > 5000) {
+        Serial.println(">>> Client Timeout !");
+        client.stop();
+        return;
+      }
     }
-    client.stop();
+ 
+    while(client.available()){
+      String line = client.readStringUntil('\r');
+      Serial.print(line);
+    }
+
+//      client.print("POST /update HTTP/1.1\n");
+//      client.print("Host: api.thingspeak.com\n");
+//      client.print("Connection: close\n");
+//      client.print("X-THINGSPEAKAPIKEY: " + apiKey + "\n");
+//      client.print("Content-Type: application/x-www-form-urlencoded\n");
+//      client.print("Content-Length: ");
+//      client.print(postStr.length());
+//      client.print("\n\n");
+//      client.print(postStr);
+    }
+    
+
+
+    client.stop(); // eh do que?
   }
 } // Fim do LOOP
